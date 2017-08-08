181ac31db0ff59cca3aaafc774c181cd9e9678d8
Leandro E. Colombo Viña <colomboleandro@bitson.com.ar>
2017-08-08 09:34:26 -0300

    Added touchpad.sh

---
+++ touchpad.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Configuring Elan Touchpad with 'xinput'
+# more info with 'xinput list-props "Elan Touchpad"
+# Natural Scrolling Enabled
+xinput set-prop 11 287 1
+# Tapping Enabled
+xinput set-prop 11 277 1
