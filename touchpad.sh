69ffcd244715648fe2aa5ca80f74af2007ba47dc
Leandro E. Colombo Viña <lecovi@zenbook>
2019-07-08 21:17:34 -0300

    Updates touchpad.sh for F30

--- touchpad.sh
+++ touchpad.sh
@@ -2,6 +2,10 @@
 # Configuring Elan Touchpad with 'xinput'
 # more info with 'xinput list-props "Elan Touchpad"
 # Natural Scrolling Enabled
-xinput set-prop 11 287 1
+xinput set-prop 11 299 1
+# Natural Scrolling Enabled Default
+#xinput set-prop 11 300 1
 # Tapping Enabled
-xinput set-prop 11 277 1
+xinput set-prop 11 291 1
+# Tapping Enabled Default
+#xinput set-prop 11 292 1
