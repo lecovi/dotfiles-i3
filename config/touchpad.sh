28c8f0bdccf972623eb66bddb541e0e507374619
Leandro E. Colombo Viña <lecovi@zenbook>
2019-08-23 09:43:53 -0300

    Updates Touchpad propierties using names

--- config/touchpad.sh
+++ config/touchpad.sh
@@ -2,10 +2,10 @@
 # Configuring Elan Touchpad with 'xinput'
 # more info with 'xinput list-props "Elan Touchpad"
 # Natural Scrolling Enabled
-xinput set-prop 11 299 1
+xinput set-prop "Elan Touchpad" "libinput Natural Scrolling Enabled" 1
 # Natural Scrolling Enabled Default
 #xinput set-prop 11 300 1
 # Tapping Enabled
-xinput set-prop 11 291 1
+xinput set-prop "Elan Touchpad" "libinput Tapping Enabled" 1
 # Tapping Enabled Default
 #xinput set-prop 11 292 1
